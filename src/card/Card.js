ab0296385eb040173b6be0a860bf9c14ceffcede
R Hema <ramakrishnagarihema@gmail.com>
2023-09-23 19:27:33 +0530

    added randomcolors for cards

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 const Card = (props) => {
   console.log(props);
   const { title, description } = props.data;
+  const [colors] = useState(["#eb4934", "#5e5c0d", "#30b828", "#163bcc"]);
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * (colors.length - 1 - 0) + 0);
+    return colors[randomIndex];
+  };
+
   return (
-    <div className="cardlist">
+    <div className="cardlist" style={{ backgroundColor: getRandomColor() }}>
       <div>
         <h1 style={{ textTransform: "uppercase" }}>{title}</h1>
         <p>{description}</p>
