3f462083cdbf7f5548f231da5a2194dae1b7da08
R Hema <ramakrishnagarihema@gmail.com>
2023-09-23 13:43:20 +0530

    added loginform and router

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return "Login Page";
+  const [login, setLogin] = useState({ password: "", email: "" });
+
+  function handleChange(e) {
+    const { name, value } = e && e.target;
+    setLogin((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(login);
+    setLogin({ email: "", password: "" });
+  }
+
+  return (
+    <div className="login-form">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input
+          className="form-input"
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Enter your Email"
+          value={login.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+        <input
+          className="form-input"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          value={login.password}
+          onChange={handleChange}
+        />
+        <button className="form-button login" type="submit">
+          Login
+        </button>
+        <button className="form-button register">
+          <Link to="/signup">Register</Link>
+        </button>
+      </form>
+    </div>
+  );
 };
 export default Login;
